095722455f627cd2c01789345dd742a340945ec3
Julián David Sánchez Gallego <50778235+juliansg022@users.noreply.github.com>
2021-10-19 16:35:03 -0500

    section-six (#3)
    * Update repo with files to create an article through a form
    
    * Update repo with files to update/edit an article through a form
    
    * Update repo with files to delete an article through a form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,53 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  # R method (CRUD)
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  # C methods (CRUD)
+  def new
+    @article =Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
+  end
+
+  # U methods (CRUD)
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+  # D methods (CRUD)
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+
 end
