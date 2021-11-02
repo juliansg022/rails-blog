bc9668f716634627bac73d7f62c6eabe603d8d63
Julián David Sánchez Gallego <julian.sanchez.kommit@gmail.com>
2021-11-02 16:48:48 -0500

    Update correct capture of the name field with devise controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,4 @@
 
 class ApplicationController < ActionController::Base
   include Pagy::Backend
-  before_action :configure_devise_params, if: :devise_controller?
-
-  def configure_devise_params
-    devise_parameter_sanitizer.permit(:sign_up) do |user|
-      user.permit(:name, :email, :password, :password_confirmation)
-    end
-  end
 end
