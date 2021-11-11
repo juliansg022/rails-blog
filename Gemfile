03ab65cb8204bc60fc16129365b62aba7b7913e5
Julián David Sánchez Gallego <julian.sanchez.kommit@gmail.com>
2021-11-11 10:12:07 -0500

    Test the articles model

--- Gemfile
+++ Gemfile
@@ -58,6 +58,8 @@ group :test do
   gem 'selenium-webdriver'
   # Easy installation and use of web drivers to run system tests with browsers
   gem 'webdrivers'
+  # Provides RSpec- and Minitest-compatible one-liners to test common Rails functionality. Read more: https://github.com/thoughtbot/shoulda-matchers
+  gem 'shoulda-matchers', '~> 5.0'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  user = User.new(id: 1, name: 'User', email: 'user@gmail.com')
+
+  subject do
+    user.articles.new(title: 'Anything',
+                      body: 'Lorem ipsum',
+                      status: 'public')
+  end
+
+  describe "Associations" do
+  it { should belong_to(:user) }
+end
+
+  describe 'Validations' do
+    it { should validate_presence_of(:user) }
+
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without a title' do
+      subject.title = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without a body' do
+      subject.body = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without a status' do
+      subject.status = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without a user' do
+      subject.user = nil
+      expect(subject).to_not be_valid
+    end
+
+  end
+
+
 end
