4a885ef18941692f34540086ec82bb47e142f9ec
Julián David Sánchez Gallego <julian.sanchez.kommit@gmail.com>
2021-11-11 13:29:45 -0500

    Update repo with rubocop applied on files

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
   include Pagy::Frontend
 
   def colombian_date(date)
-    if date.class == Time
+    if date.instance_of?(Time)
       # To convert UTC to UTC-5
       my_date = date - (60 * 60 * 5)
       # to display date as 'November 05, 2021 at 12:57:15PM'
